db5134b3661c3f8e4c5ea1c79ce6c7cfa55a57e9
Alex Chernov <alexusblack@gmail.com>
2018-01-22 14:02:04 +1100

    filtering map markers

--- js/custom-bindings/map-markers.js
+++ js/custom-bindings/map-markers.js
@@ -2,18 +2,32 @@ ko.bindingHandlers.mapMarkers = {
   update: function (element, valueAccessor, allBindingsAccessor, viewModel) {
     // Unwrapping markers list
     let placesList = ko.utils.unwrapObservable(valueAccessor());
-    console.log(placesList);
+
+    // TODO: We can have some sophisticated marker caching here if we need
 
     let createMarkers = () => {
+      // Cleaning old markers
+      if(typeof(element.markers) != 'undefined') {
+        for(let marker of element.markers) {
+          marker.setMap(null);
+        }
+      }
+      element.markers = [];
+
       for(let marker of placesList) {
         let gmarker = new google.maps.Marker({
           position: marker.location,
           map: element.googleMap,
           title: marker.name
         });
+        element.markers.push(gmarker);
       }
     };
     
-    google.maps.event.addDomListener(window, 'load', createMarkers);
+    if (document.readyState === 'complete') {
+      createMarkers();
+    } else {
+      google.maps.event.addDomListener(window, 'load', createMarkers);
+    }
   }
 };
--- js/custom-bindings/map-settings.js
+++ js/custom-bindings/map-settings.js
@@ -2,7 +2,6 @@ ko.bindingHandlers.mapSettings = {
   init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
     // Unwrapping map data
     let settings = ko.utils.unwrapObservable(valueAccessor());
-    console.log(settings);
 
     let initialize = () => element.googleMap = new google.maps.Map(element, settings);
     google.maps.event.addDomListener(window, 'load', initialize);
